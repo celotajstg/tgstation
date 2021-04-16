b84223176e0fd0416848938d96411e26f566d8be
actioninja <actioninja@criticalaction.net>
2021-04-16 04:03:57 -0700

    blenis

--- tgui/packages/tgui/interfaces/AlertModal.js
+++ tgui/packages/tgui/interfaces/AlertModal.js
@@ -6,61 +6,65 @@
 
 import { clamp01 } from 'common/math';
 import { useBackend } from '../backend';
-import { createRef } from 'inferno';
+import { Component, createRef } from 'inferno';
 import { Box, Button, Flex, Section } from '../components';
 import { Window } from '../layouts';
 import { logger } from '../logging';
-
-const inputRef = createRef();
-
-export const AlertModal = (props, context) => {
-  const { act, data } = useBackend(context);
-  const { title, message, buttons, timeout } = data;
-  if (inputRef.current) {
-    // logger.log(inputRef.current.props.px);
-    if (inputRef.current) {
-      setTimeout(() => inputRef.current.focus(), 1);
-    }
+export class AlertModal extends Component {
+  constructor() {
+    super();
+    this.buttonRef = createRef();
   }
 
-  return (
-    <Window title={title} width={350} height={150} canClose={timeout > 0}>
-      {timeout && <Loader value={timeout} />}
-      <Window.Content>
-        <Section fill>
-          <Flex direction="column" height="100%">
-            <Flex.Item grow={1}>
-              <Flex
-                direction="column"
-                className="AlertModal__Message"
-                height="100%">
-                <Flex.Item>
-                  <Box m={1}>
-                    {message}
-                  </Box>
-                </Flex.Item>
-              </Flex>
-            </Flex.Item>
-            <Flex.Item my={2}>
-              <Flex className="AlertModal__Buttons">
-                {buttons.map((button, buttonIndex) => (
-                  <Flex.Item key={buttonIndex} mx={1}>
-                    <Button
-                      ref={buttonIndex === 0 ? inputRef : null}
-                      px={3}
-                      onClick={() => act("choose", { choice: button })}>
-                      {button}
-                    </Button>
+  componentDidMount() {
+    const button = this.buttonRef.current;
+    setTimeout(() => button.focus(), 1);
+  }
+
+  render() {
+    const { act, data } = useBackend(this.context);
+    const { title, message, buttons, timeout } = data;
+
+    return (
+      <Window title={title} width={350} height={150} canClose={timeout > 0}>
+        {timeout && <Loader value={timeout} />}
+        <Window.Content>
+          <Section fill>
+            <Flex direction="column" height="100%">
+              <Flex.Item grow={1}>
+                <Flex
+                  direction="column"
+                  className="AlertModal__Message"
+                  height="100%">
+                  <Flex.Item>
+                    <Box m={1}>
+                      {message}
+                    </Box>
                   </Flex.Item>
-                ))}
-              </Flex>
-            </Flex.Item>
-          </Flex>
-        </Section>
-      </Window.Content>
-    </Window>
-  );
-};
+                </Flex>
+              </Flex.Item>
+              <Flex.Item my={2}>
+                <Flex className="AlertModal__Buttons">
+                  {buttons.map((button, buttonIndex) => (
+                    <Flex.Item key={buttonIndex} mx={1}>
+                      <Button
+                        ref={buttonIndex === 0 ? this.buttonRef : null}
+                        px={3}
+                        onClick={() => act("choose", { choice: button })}>
+                        {button}
+                      </Button>
+                    </Flex.Item>
+                  ))}
+                </Flex>
+              </Flex.Item>
+            </Flex>
+          </Section>
+        </Window.Content>
+      </Window>
+    );
+  }
+
+}
 
 export const Loader = props => {
   const { value } = props;
